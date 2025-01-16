06d4b8896f4285102798bb3582a478afe581126b
101001000 <kike.delacalle.montilla@gmail.com>
2025-01-16 15:28:50 +0100

    added next chunk of minimal as a placeholder

--- adaptor_test/minimal.cpp
+++ adaptor_test/minimal.cpp
@@ -1,13 +1,11 @@
 #include <syclbvh/adaptor.hpp>
 #include <stdio.h>
 
-void errorFunction(void* userPtr, enum RTCError error, const char* str)
-{
+void errorFunction(void* userPtr, enum RTCError error, const char* str){
   printf("error %d: %s\n", error, str);
 }
 
-RTCDevice initializeDevice()
-{
+RTCDevice initializeDevice(){
   RTCDevice device = rtcNewDevice(NULL);
 
   if (!device)
@@ -15,6 +13,41 @@ RTCDevice initializeDevice()
 
   rtcSetDeviceErrorFunction(device, errorFunction, NULL);
   return device;
+}
+
+RTCScene initializeScene(RTCDevice device){
+  RTCScene scene = rtcNewScene(device);
+
+  RTCGeometry geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
+  float* vertices = (float*) rtcSetNewGeometryBuffer(geom,
+                                                     RTC_BUFFER_TYPE_VERTEX,
+                                                     0,
+                                                     RTC_FORMAT_FLOAT3,
+                                                     3*sizeof(float),
+                                                     3);
+
+  unsigned* indices = (unsigned*) rtcSetNewGeometryBuffer(geom,
+                                                          RTC_BUFFER_TYPE_INDEX,
+                                                          0,
+                                                          RTC_FORMAT_UINT3,
+                                                          3*sizeof(unsigned),
+                                                          1);
+
+  if (vertices && indices)
+  {
+    vertices[0] = 0.f; vertices[1] = 0.f; vertices[2] = 0.f;
+    vertices[3] = 1.f; vertices[4] = 0.f; vertices[5] = 0.f;
+    vertices[6] = 0.f; vertices[7] = 1.f; vertices[8] = 0.f;
+
+    indices[0] = 0; indices[1] = 1; indices[2] = 2;
+  }
+
+  rtcCommitGeometry(geom);
+  rtcAttachGeometry(scene, geom);
+  rtcReleaseGeometry(geom);
+  rtcCommitScene(scene);
+
+  return scene;
 }
 
 int main(){
--- include/syclbvh/adaptor.hpp
+++ include/syclbvh/adaptor.hpp
@@ -1,14 +1,38 @@
+#include <cstddef> 
 
 struct RTCDevice{
     RTCDevice(const char*){};
     operator int(){return 1;};
 };
+struct RTCScene{};
+struct RTCGeometry{};
+
 enum RTCError{};
+enum RTCFormat{
+    RTC_FORMAT_FLOAT3,
+    RTC_FORMAT_UINT3
+};
+enum RTCBufferType{
+    RTC_BUFFER_TYPE_VERTEX,
+    RTC_BUFFER_TYPE_INDEX
+};
+enum RTCGeometryType{
+    RTC_GEOMETRY_TYPE_TRIANGLE
+};
+
 typedef void (*RTCErrorFunction)(
       void* userPtr,
       RTCError code,
       const char* str
     );
+
 inline RTCDevice rtcNewDevice(const char* config){ return RTCDevice(config);}
 inline int rtcGetDeviceError(RTCDevice device){ return 0;}
 inline void rtcSetDeviceErrorFunction(RTCDevice device, RTCErrorFunction error, void* userPtr){}
+inline RTCScene rtcNewScene(RTCDevice device){ return RTCScene();};
+inline RTCGeometry rtcNewGeometry(RTCDevice device, int type){ return RTCGeometry();};
+inline void* rtcSetNewGeometryBuffer(RTCGeometry geometry, RTCBufferType type, unsigned int slot, RTCFormat format, std::size_t byteStride, std::size_t itemCount){return nullptr;}
+inline void rtcCommitGeometry(RTCGeometry geometry){};
+inline void rtcAttachGeometry(RTCScene scene, RTCGeometry geometry){};
+inline void rtcReleaseGeometry(RTCGeometry geometry){};
+inline void rtcCommitScene(RTCScene scene){};
