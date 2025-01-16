152c8bc362507a2ad2c1ba9669d6ed287f6db8af
101001000 <kike.delacalle.montilla@gmail.com>
2025-01-16 15:19:57 +0100

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+rm build -rf
+mkdir build
+cd build
+cmake -S .. -DCMAKE_INSTALL_PREFIX=./install
+cmake --build . --target install
+
+cd ..
+cd adaptor_test
+rm build -rf
+mkdir build
+cd build
+syclbvh_DIR=../../build/install/lib/cmake/syclbvh cmake ..
+make
--- include/syclbvh/adaptor.hpp
+++ include/syclbvh/adaptor.hpp
@@ -1,7 +1,7 @@
 
 struct RTCDevice{
-    RTCDevice(void*){};
-    operator int(){return 0;};
+    RTCDevice(const char*){};
+    operator int(){return 1;};
 };
 enum RTCError{};
 typedef void (*RTCErrorFunction)(
@@ -9,6 +9,6 @@ typedef void (*RTCErrorFunction)(
       RTCError code,
       const char* str
     );
-inline RTCDevice rtcNewDevice(const char* config){ return RTCDevice(nullptr);}
+inline RTCDevice rtcNewDevice(const char* config){ return RTCDevice(config);}
 inline int rtcGetDeviceError(RTCDevice device){ return 0;}
 inline void rtcSetDeviceErrorFunction(RTCDevice device, RTCErrorFunction error, void* userPtr){}
